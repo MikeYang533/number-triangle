f06ef26eb8583adf5d274003139ea33fd3ee4336
billchenziang33 <billziang.chen@mail.utoronto.ca>
2025-09-23 21:46:48 +0800

    Implement loadTriangle and retrieve

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -63,7 +66,7 @@ public class NumberTriangle {
      * Note: a NumberTriangle contains at least one value.
      */
     public void maxSumPath() {
-        // for fun [not for credit]:
+        // for fun [not for credit]
     }
 
 
@@ -89,6 +92,18 @@ public class NumberTriangle {
      */
     public int retrieve(String path) {
         // TODO implement this method
+        if (path.isEmpty()) {
+            return this.root;
+        }
+
+        char direction = path.charAt(0);
+        String restPath = path.substring(1);
+
+        if (direction == 'l') {
+            return this.left.retrieve(restPath);
+        }else if (direction == 'r') {
+            return this.right.retrieve(restPath);
+        }
         return -1;
     }
 
@@ -109,6 +124,7 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
+        List<List<NumberTriangle>> levels = new ArrayList<>();
 
         // TODO define any variables that you want to use to store things
 
@@ -118,9 +134,15 @@ public class NumberTriangle {
 
         String line = br.readLine();
         while (line != null) {
-
+            String[] nums = line.trim().split("\\s+");
+            List<NumberTriangle> currentlevel = new ArrayList<>();
+            for (String num : nums){
+                int value = Integer.parseInt(num);
+                currentlevel.add(new NumberTriangle(value));
+            }
+            levels.add(currentlevel);
             // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+            //System.out.println(line);
 
             // TODO process the line
 
@@ -128,7 +150,18 @@ public class NumberTriangle {
             line = br.readLine();
         }
         br.close();
-        return top;
+
+        for (int i = 0; i < levels.size()-1 ; i++){
+            List<NumberTriangle> current = levels.get(i);
+            List<NumberTriangle> next = levels.get(i+1);
+            for (int j = 0; j < current.size(); j++){
+                current.get(j).setLeft(next.get(j));
+                current.get(j).setRight(next.get(j+1));
+            }
+        }
+
+    top = levels.get(0).get(0);
+    return top;
     }
 
     public static void main(String[] args) throws IOException {
