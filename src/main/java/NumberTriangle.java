e76e818d17217f6cc0404d7eb911ed6f7fead8e2
AquaDummy <awesomejay098@gmail.com>
2025-09-23 20:04:15 -0400

    Reformated some code

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -90,8 +90,7 @@ public class NumberTriangle {
      */
     public int retrieve(String path) {
         if (path.isEmpty()){return this.root;}
-        else{System.out.println(path.substring(1));
-            System.out.println(this.getRoot());
+        else{
             if (path.charAt(0) == 'l'){
                 return this.left.retrieve(path.substring(1));}
             else{return this.right.retrieve(path.substring(1));}
