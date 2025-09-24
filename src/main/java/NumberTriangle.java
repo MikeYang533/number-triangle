a4f56250e5a369de71d9cef31b82ffd38d16cdde
AquaDummy <awesomejay098@gmail.com>
2025-09-23 20:14:07 -0400

    Test remove import 4

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -114,9 +114,9 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-//        String[] current;
-//        ArrayList<NumberTriangle> prevTri = new ArrayList<NumberTriangle>();
-//        ArrayList<NumberTriangle> currTri = new ArrayList<NumberTriangle>();
+        String[] current;
+        ArrayList<NumberTriangle> prevTri = new ArrayList<NumberTriangle>();
+        ArrayList<NumberTriangle> currTri = new ArrayList<NumberTriangle>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
