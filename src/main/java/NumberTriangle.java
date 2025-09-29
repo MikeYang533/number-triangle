460ba4f87187e210f0c3407d2d500e40b8f422cc
AveryThompson7 <avery.thompson@mail.utoronto.ca>
2025-09-29 13:56:54 -0400

    Completed retrieve method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,7 @@
+import com.sun.source.tree.NewArrayTree;
+
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -88,7 +91,20 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
+
+        NumberTriangle curr = null;
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 'l' ){
+                curr = this.left;
+            }
+            else if (path.charAt(i) == 'r' ){
+                curr = this.right;
+            }
+        }
+        if (curr != null) {
+            return curr.getRoot();
+        }
+
         return -1;
     }
 
@@ -109,8 +125,7 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
+        //TODO: initialize variables to store thing
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
@@ -118,12 +133,7 @@ public class NumberTriangle {
 
         String line = br.readLine();
         while (line != null) {
-
-            // remove when done; this line is included so running starter code prints the contents of the file
             System.out.println(line);
-
-            // TODO process the line
-
             //read the next line
             line = br.readLine();
         }
