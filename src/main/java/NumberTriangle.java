de574d1752d230d3b76a0957e85a4e5a7d9557a6
Jeff Liang <jefflyf2006@163.com>
2025-09-29 01:23:10 -0400

    delete all todos

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -123,8 +123,6 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
         List<List<Integer>> rows = new ArrayList<>();
 
         // will need to return the top of the NumberTriangle,
