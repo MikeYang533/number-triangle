86233845932dedbfc6f718675bde7b5aeefcebc6
Hanze Guan <hanze.guan@mail.utoronto.ca>
2025-09-29 10:21:59 -0400

    solve the constructor

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -109,21 +110,33 @@ public class NumberTriangle {
         InputStream inputStream = NumberTriangle.class.getClassLoader().getResourceAsStream(fname);
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
-
-        // TODO define any variables that you want to use to store things
+        ArrayList<NumberTriangle> perRow = new ArrayList<>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
         NumberTriangle top = null;
 
         String line = br.readLine();
+
         while (line != null) {
 
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+            ArrayList<NumberTriangle> curRow = new ArrayList<>();
+            String [] parts = line.trim().split("\\s+");
+            for (String number : parts){
+                curRow.add(new NumberTriangle(Integer.parseInt(number)));
+            }
 
-            // TODO process the line
+            if (top == null) {
+                top = curRow.get(0);
+            }
+            else{
+                for (int i = 0; i < perRow.size(); i++){
+                    perRow.get(i).setLeft(curRow.get(i));
+                    perRow.get(i).setRight(curRow.get(i + 1));
+                }
+            }
 
+            perRow = curRow;
             //read the next line
             line = br.readLine();
         }
