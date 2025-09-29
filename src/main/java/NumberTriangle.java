e0924f0b68ce0420fe398a6f156cce95c6b0b673
Jeff Liang <jefflyf2006@163.com>
2025-09-29 00:42:57 -0400

    fix retrieve

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,8 +88,21 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        // base case, what if path is empty
+        if(path.isEmpty()){
+            return this.root;
+        }
+        // recursive case
+        else{
+            if(path.charAt(0) == 'l'){
+                // if it is left
+                return this.left.retrieve(path.substring(1));
+            }
+            else{
+                // if it is right
+                return this.right.retrieve(path.substring(1));
+            }
+        }
     }
 
     /** Read in the NumberTriangle structure from a file.
