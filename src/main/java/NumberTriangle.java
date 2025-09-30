76f08795f5228bab1e6c4abcb10908ec115ddbe3
Olivia <olivia.markow@mail.utoronto.ca>
2025-09-30 15:23:52 -0400

    finished retrieve

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -93,7 +93,17 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
+        for (String c : path.split("")){
+            if (c.equals("l")){
+                if (left != null){
+                    return left.retrieve(path.substring(1));
+                }
+            } else if (c.equals("r")){
+                if (right != null){
+                    return right.retrieve(path.substring(1));
+                }
+            }
+        }
         return -1;
     }
 
