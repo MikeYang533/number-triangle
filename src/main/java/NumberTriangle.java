f8cd11f4cecafdb416610b3656b27e8d57361ee5
jaydenisnthigh <jaydenluo512@gmail.com>
2025-10-02 20:24:26 -0400

    finished loadtriangle static method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is the provided NumberTriangle class to be used in this coding task.
@@ -110,7 +112,7 @@ public class NumberTriangle {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
 
-        // TODO define any variables that you want to use to store things
+        List<NumberTriangle> prevRow = new ArrayList<>();
 
         // will need to return the top of the NumberTriangle,
         // so might want a variable for that.
@@ -119,10 +121,28 @@ public class NumberTriangle {
         String line = br.readLine();
         while (line != null) {
 
-            // remove when done; this line is included so running starter code prints the contents of the file
-            System.out.println(line);
+            line = line.trim();
+            if (!line.isEmpty()) {
+                String[] nums = line.split("\\s+");
+                List <NumberTriangle> currentRow = new ArrayList<>();
 
-            // TODO process the line
+                for (int i = 0; i < nums.length; i++) {
+                    NumberTriangle node = new NumberTriangle(Integer.parseInt(nums[i]));
+                    currentRow.add(node);
+
+                    if (!prevRow.isEmpty()){
+                        if (i > 0) {
+                            prevRow.get(i-1).setRight(node);
+                        }
+                        if (i < prevRow.size()){
+                            prevRow.get(i).setLeft(node);
+                        }
+                    } else {
+                        top = node;
+                    }
+                }
+                prevRow = currentRow;
+            }
 
             //read the next line
             line = br.readLine();
